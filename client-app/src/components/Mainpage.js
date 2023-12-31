659310c624ea3420b4e953543499a7999cef3b18
Harekrushn Tejani <harekrushntejani64@gmail.com>
2023-12-31 20:15:13 +0530

    final touch

--- client-app/src/components/Mainpage.js
+++ client-app/src/components/Mainpage.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react'
 import classes from './Mainpage.module.css'
 
 const Mainpage = () => {
+    // State variables to manage input, result, and loading state.
     const [url, setUrl] = useState('');
     const [result, setResult] = useState(null);
-    const [valid, setValid] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    // Function to handle form submission and communicate with the server.
     const handleSubmit = async () => {
+        // Set loading state to true and reset result.
         setLoading(true);
+        setResult(null);
 
+        // Make a POST request to the server with the provided Wikipedia URL.
         const response = await fetch(`http://localhost:5000/wikipedialoopchecker`, {
             method: "POST",
             headers: {
@@ -19,26 +23,27 @@ const Mainpage = () => {
                 url: url,
             })
         });
-
+        // Parse the response as JSON.
         const data = await response.json();
-        // console.log(data);
 
-        if (data.success === 1) {
-            setValid(true);
-        } else {
-            setValid(false);
-        }
+        // Set the result and reset loading state.
         setResult(data);
-
-        setUrl("");
         setLoading(false);
     }
+
+    // Function to handle clearing the input and result.
+    const handleClear = () => {
+        setUrl('');
+        setResult(null);
+    };
 
     return (
         <>
             <div className={classes.container}>
                 <div className={classes.main}>
-                    <h1 className={classes.heading}>Wikipedia Loop Checker</h1>
+                    <h1 className={classes.heading}>
+                        Wikipedia Loop Checker
+                    </h1>
                     <div className={classes.inputdiv}>
                         <label>
                             Wikipedia URL:
@@ -48,43 +53,56 @@ const Mainpage = () => {
                             value={url}
                             onChange={(e) => setUrl(e.target.value)}
                             placeholder='Enter Wikipedia URL'
-                            onFocus={() => setValid(null)}
                         />
                     </div>
-                    <button onClick={handleSubmit} disabled={loading}>
+                    <button onClick={handleSubmit} disabled={loading} className={classes.checkButton}>
                         {loading ? 'Checking Loop...' : 'Check Loop'}
+                    </button>
+                    <button onClick={handleClear} className={classes.clearButton}>
+                        Clear
                     </button>
                 </div>
 
-                {valid && (
+                {/* Display results if available */}
+                {result && (
                     <div className={classes.resultsContainer}>
-                        <p className={classes.requesttext}>Total Requests :
-                            <span className={classes.requestNumber}>
-                                {result.count}
-                            </span>
-                        </p>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Sr No.</th>
-                                    <th>Page</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {result.visitedPages.map((link, index) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{link}</td>
+                        {result.success === 1 ? (
+                            // Display total requests if the loop check is successful
+                            <div className={classes.requestContainer}>
+                                <p className={classes.requesttext}>
+                                    Total Requests :
+                                    <span className={classes.requestNumber}>
+                                        {result.count}
+                                    </span>
+                                </p>
+                            </div>
+                        ) : (
+                            // Display error message if the loop check fails
+                            <div className={classes.errorContainer}>
+                                <div className={classes.errorHeading}>Error : </div>
+                                <p className={classes.errorMessage}>{result?.message}</p>
+                            </div>
+                        )}
+
+                        {/* Display visited pages if available */}
+                        {result.visitedPages && result.visitedPages.length > 0 && (
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Sr No.</th>
+                                        <th>Page</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-                {valid === false && (
-                    <div className={classes.errorContainer}>
-                        <div className={classes.errorHeading}>Error : </div>
-                        <p className={classes.errorMessage}>{result?.message}</p>
+                                </thead>
+                                <tbody>
+                                    {result.visitedPages.map((link, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{link}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
                     </div>
                 )}
             </div>
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cheerio = require('cheerio');
 const cors = require('cors');
-
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const app = express();
@@ -11,29 +11,32 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+// Endpoint for handling Wikipedia loop checks
 app.post('/wikipedialoopchecker', async (req, res) => {
-    // console.log(req.body);
     const url = req.body.url;
 
+    // Check if the provided URL is a valid Wikipedia URL
     if (!url.match('https://en.wikipedia.org/wiki/')) {
         res.json({
             success: 0, message: 'Please enter a valid wikipedia URL !!'
         });
     } else {
+        // Perform the loop check and send the result in the response
         const result = await checkLoop(url);
-        console.log(result);
-
         res.json(result);
     }
 
 });
 
+// Function to perform the Wikipedia loop check
 async function checkLoop(url) {
     const visitedPages = [];
     let currentUrl = url;
     let count = 0;
 
+    // Continue loop until reaching the Philosophy page or detecting a loop
     while (currentUrl.toLowerCase() !== 'https://en.wikipedia.org/wiki/philosophy') {
+        // If the page is already visited, return an error
         if (visitedPages.includes(currentUrl)) {
             return {
                 success: 0, message: "Loop Detected !! A page that is already visited !"
@@ -42,21 +45,28 @@ async function checkLoop(url) {
 
         visitedPages.push(currentUrl);
 
+        // Fetch the content of the current page
         const pageData = await fetch(currentUrl);
         const pageContent = await pageData.text();
+
+        // Get the first valid link from the page content
         currentUrl = getNextLink(pageContent);
 
+        // If no valid URL is found, return an error
         if (!currentUrl) {
             return {
                 success: 0, message: "No Valid URL find in Loop"
             }
         }
 
+        // Increment the request count
         count++;
     }
 
+    // Add the Philosophy page to the visited pages array
     visitedPages.push(currentUrl);
 
+    // Return the result of the loop check
     return {
         count,
         visitedPages,
@@ -65,6 +75,7 @@ async function checkLoop(url) {
     };
 }
 
+// Function to extract the first valid link from the page content
 function getNextLink(pageContent) {
     const $ = cheerio.load(pageContent);
     const paragraphs = $('#bodyContent p');
@@ -76,6 +87,7 @@ function getNextLink(pageContent) {
         for (let j = 0; j < links.length; j++) {
             const link = $(links[j]);
 
+            // Check if the link is not within a span element
             if (!link.parents().filter('span').length) {
                 const href = link.attr('href');
                 console.log(href);
@@ -87,9 +99,11 @@ function getNextLink(pageContent) {
         }
     }
 
+    // Return null if no valid URL is found
     return null;
 }
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
